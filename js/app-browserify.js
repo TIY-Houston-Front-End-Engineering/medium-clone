a864041050426bad1398eef7c06b0153680ecc91
paulesaad <paulsaad.13@gmail.com>
2015-06-30 04:15:09 -0500

    login functionalization

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -13,7 +13,7 @@ var Parse = window.Parse
 import {PostStory, PostStoryList} from './post'
 
 //Profile View:
-import {frontOfCoin} from './frontOfCoin'
+// import {LoginView} from './login'
 
 Parse.$ = $
 Parse.initialize(`sR0yX3XlZSVU0nt7eFPKSITMJT3uwRJVnl3HMKMT`, `sv8ImqvJytt5B3lMyf2NwURN1iQUz54Gg2rGDbuF`)
@@ -21,6 +21,20 @@ Parse.initialize(`sR0yX3XlZSVU0nt7eFPKSITMJT3uwRJVnl3HMKMT`, `sv8ImqvJytt5B3lMyf
 var qs = (selector) => document.querySelector(selector) 
 
 const stories = new PostStoryList()
+
+class SvgLogin extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<span className="icon">
+				<svg version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M65.768,26.221v6.836h-5.18l6.107,36.226c0,0-13.58,11.996-14.393,26.85   c0.211,0.379,0.33,0.813,0.33,1.275c0,1.291-0.928,2.363-2.153,2.59V69.512c1.43-0.23,2.522-1.469,2.522-2.963   c0-1.658-1.344-3.002-3.001-3.002c-1.658,0-3.003,1.344-3.003,3.002c0,1.498,1.099,2.74,2.535,2.965V100   c-1.231-0.221-2.166-1.297-2.166-2.592c0-0.463,0.12-0.896,0.33-1.275c-0.812-14.854-14.393-26.85-14.393-26.85l6.108-36.226h-5.18   v-6.836h2.412L33.782,0h21.361l-0.596,24.202h4.213L60.146,0h6.072l-2.863,26.221H65.768z M59.094,26.469h-6.156v6.213h6.156   V26.469z M54.641,35.157l2.635,43.284c0,0,1.207-1.83,2.617-3.672c1.314-1.717,2.84-3.453,2.84-3.453l-3.865-36.166L54.641,35.157z"></path></g></svg>
+				<span>Log In or Sign-Up</span>
+			</span>
+		)
+	}
+}
 
 class Toolbar extends Component{
 	constructor(props){
@@ -119,6 +133,7 @@ class ProfileView extends Component {
 	}
 
 	render() { 
+		Parse.User.logOut()
 		return (<div>
 			<Toolbar />
 			<form> 
@@ -188,9 +203,9 @@ class PostListView extends Component{
 class LoginView extends Component{
 	constructor(props){
 		super(props)
-		this.state = {
-			error: 0
-		}
+		// this.state = {
+		// 	error: 0
+		// }
 	}
 
 	_registerUser(e){
@@ -237,26 +252,44 @@ class LoginView extends Component{
 	}
 
 	render(){
-		return(<div>
+		return(
 			<div className="rotationContainer">
-				<frontOfCoin />
+				<div className="frontOfCoin">
+					<div>
+						<h1>Mi・lieu</h1>
+						<span>noun</span>
+					</div>
+					<span className="def">a social setting in which something occurs or develops.</span>
+					<SvgLogin />
+				</div>
+				<div className="backOfCoin">
+					<div className="login">
+						<h4>Login</h4>
+						<form>
+							<div>
+								<input type="text" ref="username" placeholder="Enter username"/>
+								<input type="password" ref="password" placeholder="Enter password"/>
+							</div>
+							<button onClick={(e)=> this._signIn(e)}>Sign In</button>
+						</form>
+					</div>
+					<div className="signup">
+						<h4>Sign Up</h4>
+						<form>
+							<div>
+								<span><input type="text" ref="firstname" placeholder="First Name"/>
+								<input type="text" ref="lastname" placeholder="Last Name"/></span>
+							</div>
+							<div>
+								<span><input type="email" ref="email" placeholder="Email"/>
+								<input type="text" ref="newUsername" placeholder="Username"/></span>
+							</div>
+							<div><input type="password" ref="newPassword" placeholder="Password"/></div>
+							<button onClick={(e) => this._registerUser(e)}>Join the Charge</button>
+						</form>
+					</div>
+				</div>
 			</div>
-			<h3>Login Here</h3>
-			<form>
-				Enter Username: <input type="text" ref="username" />
-				Enter Password: <input type="password" ref="password" />
-				<button onClick={(e)=> this._signIn(e)}>Sign In</button>
-			</form>
-			<h3>Sign Up</h3>
-			<form>
-				First Name: <input type="text" ref="firstname" />
-				Last Name: <input type="text" ref="lastname" />
-				Email: <input type="email" ref="email" />
-				Username: <input type="text" ref="newUsername" />
-				Password: <input type="password" ref="newPassword" />
-				<button onClick={(e) => this._registerUser(e)}>Join the Charge</button>
-			</form>
-		</div>
 		)
 	}
 }
@@ -283,6 +316,7 @@ var ParseRouter = Parse.Router.extend({
 			return
 		}
 		stories.fetch()
+		// React.render(<frontOfCoin />, qs('.container'))
 		React.render(<PostListView storedPosts={stories} />, qs('.container'))
 	},
 
